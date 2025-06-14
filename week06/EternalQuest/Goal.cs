8a08b9ccc122c9fecf510731af503c6dd2633083
United-Statess <keanucharleskeanu01@gmail.com>
2025-06-14 09:35:01 +0100

    "Update Week06 "

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -11,7 +11,6 @@ public abstract class Goal
         _points = points;
     }
 
-
     public abstract int RecordEvent();
     public abstract bool IsComplete();
     public abstract string GetStatus();
